eb586bcd351382d5df1aae95cc9c554b81d19db8
Mathieu Szydlowski <mathieu.lowski@gmail.com>
2021-03-22 14:24:18 -0400

    fixed untracked files

--- server/config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports={
-    MONGOURI:"mongodb+srv://mathieu:y5m1spWqkDvmbjOl@projetavocat.qpk35.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    JWT_SECRET:"DeszczeNieSpokojnePotargamyPsaAmyNaTejWojnieLadnychPareLatDoDomuWrocimyWpiecuNapalimy"
-}
-//y5m1spWqkDvmbjOl
